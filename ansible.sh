29391c845c4aad0665a9a15aace7ea9483366a2f
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-08-19 12:30:01 +0200

    Add possibility to choose ansible version

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,7 @@
+VERSION=${1:-2.9.0}
+
 sudo apt-get install update -y
 sudo apt-get install git -y
 sudo apt-get install gpg -y
-sudo apt-get install ansible
+sudo apt-get install pip
+sudo pip install ansible==${VERSION}
